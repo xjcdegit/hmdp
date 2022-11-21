953d70d99eeac8ae6ae79896734c16b3b8161a4d
itcast <hello@itcast.cn>
2022-11-21 09:29:04 +0800

    hmdp

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
@@ -65,19 +65,21 @@ public class ShopController{
      * 根据商铺类型分页查询商铺信息
      * @param typeId 商铺类型
      * @param current 页码
+     * @param x 经度
+     * @param y 纬度
+     * required = false 即可有可无该参数
      * @return 商铺列表
      */
     @GetMapping("/of/type")
     public Result queryShopByType(
             @RequestParam("typeId") Integer typeId,
-            @RequestParam(value = "current", defaultValue = "1") Integer current
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam(value = "x",required = false) Double x,
+            @RequestParam(value = "y",required = false) Double y
     ) {
         // 根据类型分页查询
-        Page<Shop> page = shopService.query()
-                .eq("type_id", typeId)
-                .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
-        // 返回数据
-        return Result.ok(page.getRecords());
+        // 如果没有传x y，就直接查询数据库
+        return shopService.queryShopByType(typeId,current,x,y);
     }
 
     /**
